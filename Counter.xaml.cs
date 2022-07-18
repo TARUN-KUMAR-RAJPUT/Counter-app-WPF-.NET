e63ad9609fd3094f518d6a0f001258a1b9c72cb0
Tarun Kumar Rajput <your>
2022-07-18 09:26:01 +0530

    Files added

---
+++ Counter.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CounterApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        int n = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void click_Click(object sender, RoutedEventArgs e)
+        {
+            n++;
+            textbx.Text = n.ToString();
+        }
+
+        private void resetBt_Click(object sender, RoutedEventArgs e)
+        {
+            n = 1;
+            textbx.Text = n.ToString();
+        }
+    }
+}
